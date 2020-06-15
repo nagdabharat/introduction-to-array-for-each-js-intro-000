6ee8ebc2d1906279223a2d6f9865c897528ae2ea
Bharat Nagda <nagdabharat@gmail.com>
2020-06-15 13:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray(callback, array) {
-  array.forEach(callback)
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
 }
 
 // Add your changeCompletely() function here:
